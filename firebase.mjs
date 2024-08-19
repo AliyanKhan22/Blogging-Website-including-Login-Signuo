8f7f25c05362791cfa0d4f2aeb3aea21a6b27d87
AliyanKhan22 <alyanwasim81@gmail.com>
2024-08-19 15:49:38 -0700

    Blogging Web

--- firebase.mjs
+++ firebase.mjs
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getFirestore ,collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getFirestore ,collection, addDoc} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 const firebaseConfig = {
   apiKey: "AIzaSyD6OwZmo-n8sLm6d48JSyzdjuOKQ9KfKvU",
   authDomain: "first-project-17489.firebaseapp.com",
